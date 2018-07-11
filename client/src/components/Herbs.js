9e230412999cc385e308e36fa845529b92491a8a
github username <github email address>
2018-07-11 17:09:04 -0400

    working on like feature

--- client/src/components/Herbs.js
+++ client/src/components/Herbs.js
@@ -1,23 +1,66 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
-const Herbs = (props) => {
-  let herbs = null;
-
-  if (props.herbs) {
-    herbs = props.herbs.map(herb =>
-      <h4 key={herb.id}><li role='presentation'><Link to={`${props.url}/${herb.id}`}>{herb.name}</Link></li></h4>
-    )
+class Herbs extends React.Component {
+  state = {
+    likes: []
   }
 
-  return (
-    <div className="col-sm-3">
-      <ul className="nav nav-pills nav-stacked">
-        {herbs}
-      </ul>
-    </div>
-  )
+  counterHandler = (event) => {
+    debugger;
+    const herbId = event.target.dataset.id
+    
+  }
+
+  render() {
+    let herbs = null;
+
+    if (this.props.herbs) {
+      herbs = this.props.herbs.map(herb =>
+        <h4 key={herb.id}>
+          <li role='presentation'>
+            <Link to={`${this.props.url}/${herb.id}`}>{herb.name}</Link>
+            <span> </span>
+            <button type='button' data-id={herb.id} onClick={event => this.counterHandler(event)}>
+              {this.state.counter}
+            </button>
+          </li>
+        </h4>
+      );
+    }
+
+    return (
+      <div className="col-sm-3">
+        <ul className="nav nav-pills nav-stacked">
+          {herbs}
+        </ul>
+      </div>
+    )
+  }
 }
+
+
+// const Herbs = (props) => {
+//   let herbs = null;
+//
+//   if (props.herbs) {
+//     herbs = props.herbs.map(herb =>
+//       <h4 key={herb.id}>
+//         <li role='presentation'>
+//           <Link to={`${props.url}/${herb.id}`}>{herb.name}</Link>
+//           <button type='button' onClick={increaseCount}></button>
+//         </li>
+//       </h4>
+//     )
+//   }
+//
+//   return (
+//     <div className="col-sm-3">
+//       <ul className="nav nav-pills nav-stacked">
+//         {herbs}
+//       </ul>
+//     </div>
+//   )
+// }
 
 export default Herbs;
--- client/src/reducers/herbs.js
+++ client/src/reducers/herbs.js
@@ -13,7 +13,7 @@ export default (state = {loading: false, herbs: [], target: [], error: null}, ac
         herb.name.includes(formattedInput.trim()) || ( herb.medicinal_uses && herb.medicinal_uses.includes(formattedInput.trim()) ) || (herb.properties && herb.properties.includes(formattedInput.trim()))
       );
 
-      return Object.assign({}, state, {target: matchingHerbs})
+      return Object.assign({}, state, { target: matchingHerbs })
     case 'MEDICINAL_USE':
       const medicinalUse = action.input.split(' ').map(
         w => w.charAt(0).toUpperCase() + w.substr(1)
@@ -22,7 +22,7 @@ export default (state = {loading: false, herbs: [], target: [], error: null}, ac
       const matches = state.herbs.filter(herb =>
         herb.medicinal_uses && herb.medicinal_uses.includes(medicinalUse.trim())
       );
-    
+
       return Object.assign({}, state, {target: matches})
     case 'ERROR':
       return Object.assign({}, state, {error: action.error})
