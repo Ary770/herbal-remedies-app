a599812890901da82a898fdbc9a19eb9119f9604
github username <github email address>
2018-07-13 16:25:57 -0400

    updated search feature

--- client/src/reducers/herbs.js
+++ client/src/reducers/herbs.js
@@ -12,33 +12,25 @@ export default (state = {
       return Object.assign({}, state, {loading: false, herbs: action.herbs, target: action.herbs})
 
     case 'SEARCH_HERBS':
-      const formattedInput = action.input.split(' ').map(
-        w => w.charAt(0).toUpperCase() + w.substr(1)
-      ).join(' ');
+      const formattedInput = action.input.toLocaleLowerCase();
 
       const matchingHerbs = state.herbs.filter(herb =>
-        herb.name.includes(formattedInput.trim()) || ( herb.medicinal_uses && herb.medicinal_uses.includes(formattedInput.trim()) ) || (herb.properties && herb.properties.includes(formattedInput.trim()))
+        herb.name.toLocaleLowerCase().includes(formattedInput.trim()) || ( herb.medicinal_uses && herb.medicinal_uses.toLocaleLowerCase().includes(formattedInput.trim()) ) || (herb.properties && herb.properties.toLocaleLowerCase().includes(formattedInput.trim()))
       );
       return Object.assign({}, state, { target: matchingHerbs })
-
-    case 'MEDICINAL_USE':
-      const medicinalUse = action.input.split(' ').map(
-        w => w.charAt(0).toUpperCase() + w.substr(1)
-      ).join(' ');
-
-      const matches = state.herbs.filter(herb =>
-        herb.medicinal_uses && herb.medicinal_uses.includes(medicinalUse.trim())
-      );
-      return Object.assign({}, state, { target: matches })
 
     case 'ERROR':
       return Object.assign({}, state, { error: action.error })
 
     case 'UPDATE_LIKE':
-      const index = state.target.findIndex(herb => herb.id === action.herb.id);
-      const updateHerbs = [...state.target];
-      updateHerbs[index] = action.herb;
-      return Object.assign({}, state, { target: updateHerbs }, { herbs: updateHerbs });
+      const herbsIndex = state.herbs.findIndex(herb => herb.id === action.herb.id);
+      const targetIndex = state.target.findIndex(herb => herb.id === action.herb.id);
+      const updateHerbs = [...state.herbs];
+      const updateTarget = [...state.target];
+
+      updateHerbs[herbsIndex] = action.herb;
+      updateTarget[targetIndex] = action.herb;
+      return Object.assign({}, state, { target: updateTarget }, { herbs: updateHerbs });
 
     case 'RESET_HERBS':
       return Object.assign({}, state, { target: state.herbs })
