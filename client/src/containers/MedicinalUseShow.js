755458e05f4941fcbbe40224f0e134befb19a9fa
github username <github email address>
2018-07-23 18:16:19 -0400

    removed unused commented code

--- client/src/containers/MedicinalUseShow.js
+++ client/src/containers/MedicinalUseShow.js
@@ -21,28 +21,6 @@ const MedicinalUseShow = (props) => {
   )
 }
 
-// class MedicinalUseShow extends React.Component {
-//
-//   render () {
-//     let displayHerbs = null;
-//
-//     if (this.props.herbs.length !== 0 ) {
-//       //when user clicks on link, route defined on MedicinalUsesPage renders HerbShow component.
-//       displayHerbs = this.props.herbs.map(herb =>
-//         <li key={herb.id}>
-//           <Link to={`${this.props.url}/herbs/${herb.id}`} className='li-16px-style'>{herb.name}</Link>
-//         </li>
-//       )
-//     }
-//
-//     return (
-//       <ul>
-//         {displayHerbs}
-//       </ul>
-//     )
-//   }
-// }
-
 const mapStateToProps = (state, ownProps) => {
   return({
     herbs: state.medicinalUses.herbs
