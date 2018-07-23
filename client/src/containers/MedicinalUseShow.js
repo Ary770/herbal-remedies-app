c1b8d410646a182e6855d91ec616b30db9719eb8
github username <github email address>
2018-07-23 18:15:44 -0400

    turned into functional component, added relevant comments

--- client/src/containers/MedicinalUseShow.js
+++ client/src/containers/MedicinalUseShow.js
@@ -1,38 +1,47 @@
 import React from 'react';
-import '../App.css';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-// import PanelWrapper from '../components/PanelWrapper';
-// import Herbs from '../components/Herbs'
-// import { updatePath } from '../utilities/functions'
-// import Aux from '../HOC/Aux'
-// import { bindActionCreators } from 'redux';
-// import * as actions from '../actions/medicinalUses';
 
-class MedicinalUseShow extends React.Component {
-  displayPanelHandler(event, herb) {
-    console.log(herb)
-  }
+const MedicinalUseShow = (props) => {
+  let displayHerbs = null;
 
-  render () {
-    let displayHerbs = null;
-    
-    if (this.props.herbs.length !== 0 ) {
-      //when user clicks on link, bring up Herb Panel with all herb properties, medicinal uses etc. Don't worry about routing at this point, just get it to work.
-      displayHerbs = this.props.herbs.map(herb =>
-        <li key={herb.id}>
-          <Link to={`${this.props.url}/herbs/${herb.id}`} className='li-16px-style'>{herb.name}</Link>
-        </li>
-      )
-    }
-    //display bigger herb list
-    return (
-      <ul>
-        {displayHerbs}
-      </ul>
+  if (props.herbs.length !== 0 ) {
+    //when user clicks on link, route defined on MedicinalUsesPage renders HerbShow component.
+    displayHerbs = props.herbs.map(herb =>
+      <li key={herb.id}>
+        <Link to={`${props.url}/herbs/${herb.id}`} className='li-16px-style'>{herb.name}</Link>
+      </li>
     )
   }
+
+  return (
+    <ul>
+      {displayHerbs}
+    </ul>
+  )
 }
+
+// class MedicinalUseShow extends React.Component {
+//
+//   render () {
+//     let displayHerbs = null;
+//
+//     if (this.props.herbs.length !== 0 ) {
+//       //when user clicks on link, route defined on MedicinalUsesPage renders HerbShow component.
+//       displayHerbs = this.props.herbs.map(herb =>
+//         <li key={herb.id}>
+//           <Link to={`${this.props.url}/herbs/${herb.id}`} className='li-16px-style'>{herb.name}</Link>
+//         </li>
+//       )
+//     }
+//
+//     return (
+//       <ul>
+//         {displayHerbs}
+//       </ul>
+//     )
+//   }
+// }
 
 const mapStateToProps = (state, ownProps) => {
   return({
