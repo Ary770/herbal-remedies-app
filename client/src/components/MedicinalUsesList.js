6ca489bf5de01244e156d2221b8ed78e2255be98
github username <github email address>
2018-07-23 18:01:00 -0400

    turned medicinalUsesList into functional component

--- client/src/components/MedicinalUsesList.js
+++ client/src/components/MedicinalUsesList.js
@@ -7,50 +7,34 @@ import { bindActionCreators } from 'redux';
 import * as actions from '../actions/medicinalUses';
 // import Alert from './Alert'
 
-class MedicinalUsesList extends React.Component {
-  // state = {
-  //   muId: ''
-  // }
+const MedicinalUsesList = (props) => {
+  let medicinalUses = null;
 
-  // displayHerbsHandler = (medicinalUse) => {
-  //   //set state.herbs = to medicinalUse.herbs
-  //   //set state.displayHerbs =  !state.displayHerbs
-  //   this.setState({
-  //     displayHerbs: !this.displayHerbs,
-  //   })
-  // }
-
-  render() {
-    let medicinalUses = null;
-  
-    if (this.props.medicinalUses) {
-      medicinalUses = this.props.medicinalUses.map(medicinalUse => {
-        return (
-          <Aux key={medicinalUse.id}>
-            <h4>
-              <li role='presentation'>
-                <a href='/' onClick={(event) => this.props.fetchHerbsHandler(event, medicinalUse.id)}>
-                  {medicinalUse.name}
-                </a>
-                <span>  </span>
-              </li>
-            </h4>
-            { this.props.muId === medicinalUse.id ? <MedicinalUseShow url={this.props.url}/> : null}
-          </Aux>
-        )
-      });
-    }
-
-    // { this.state.showHerbs ? <MedicinalUseShow herbs={this.state.mU.herbs}/> : null}
-    // { this.state.muId === medicinalUse.id ? <MedicinalUseShow herbs={medicinalUse.herbs}/> : null}
-    return (
-      <div className="col-sm-5">
-        <ul className="nav nav-pills nav-stacked">
-          { medicinalUses }
-        </ul>
-      </div>
-    )
+  if (props.medicinalUses) {
+    medicinalUses = props.medicinalUses.map(medicinalUse => {
+      return (
+        <Aux key={medicinalUse.id}>
+          <h4>
+            <li role='presentation'>
+              <a href='/' onClick={(event) => props.fetchHerbsHandler(event, medicinalUse.id)}>
+                {medicinalUse.name}
+              </a>
+              <span>  </span>
+            </li>
+          </h4>
+          { props.muId === medicinalUse.id ? <MedicinalUseShow url={props.url}/> : null}
+        </Aux>
+      )
+    });
   }
+
+  return (
+    <div className="col-sm-5">
+      <ul className="nav nav-pills nav-stacked">
+        { medicinalUses }
+      </ul>
+    </div>
+  )
 }
 
 const mapStateToProps = state => {
