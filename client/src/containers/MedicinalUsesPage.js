4c583bab3458d8f957a9ce7c890a642661ee3a94
github username <github email address>
2018-07-24 14:26:47 -0400

    moved MedicinalUsesList to conatiners folder

--- client/src/containers/MedicinalUsesPage.js
+++ client/src/containers/MedicinalUsesPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import MedicinalUsesList from '../components/MedicinalUsesList';
+import MedicinalUsesList from './MedicinalUsesList';
 import HerbShow from './HerbShow';
 // import Alert from '../components/Alert'
 import { Route } from 'react-router-dom';
