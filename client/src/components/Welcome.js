23e820f362ed63e022c09ba6ab9c42a27c0b1226
github username <github email address>
2018-07-24 15:36:04 -0400

    made minor changes to text

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -7,15 +7,14 @@ const Welcome = () =>
   <h1>Welcome To Herbal Remedies</h1>
 
     <blockquote className="blockquote">
-      <p className="mb-0">No Illness which can be treated by the diet should be treated by any other means.
+      <p className="mb-0">
+        "No Illness which can be treated by diet should be treated by any other means."
       </p>
       <footer className="blockquote-footer">Maimonides</footer>
     </blockquote>
   <p>
     <NavLink to='/herbs'>Top 65 Most Commonly Used Herbs</NavLink>
   </p>
-
 </Jumbotron>
-
 
 export default Welcome
