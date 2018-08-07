87ca8040fb3f0ee7a1f981e43dbfb9e3c1b231af
github username <github email address>
2018-08-07 14:34:47 -0400

    added fallback_index_html for react-router and heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   scope '/api' do
-
     resources :herbs
     get '/likes', to: 'herbs#likes'
 
     resources :medicinal_uses
 
     resources :properties
+  end
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
